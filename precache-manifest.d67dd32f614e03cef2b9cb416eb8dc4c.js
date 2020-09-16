8e6dc69724f7c1eee87f665b8e10ef03a395fd00
RI-yurievicch <ir.1997.11.22@gmail.com>
2020-09-17 00:04:07 +0300

    Updates

---
+++ precache-manifest.d67dd32f614e03cef2b9cb416eb8dc4c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88d00574006561d345bdd76043f2a6b",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "0cf6da5e61e454fbe23d",
+    "url": "/Pokedex/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "b9900621d2a88d3be0ba",
+    "url": "/Pokedex/static/css/main.cdf0a2db.chunk.css"
+  },
+  {
+    "revision": "0cf6da5e61e454fbe23d",
+    "url": "/Pokedex/static/js/2.9e4dfbbf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Pokedex/static/js/2.9e4dfbbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9900621d2a88d3be0ba",
+    "url": "/Pokedex/static/js/main.ea2d3135.chunk.js"
+  },
+  {
+    "revision": "4a03611bc8a35df2cd71",
+    "url": "/Pokedex/static/js/runtime-main.896a5d6c.js"
+  },
+  {
+    "revision": "bcde4333918b080136c1be04bf4a5f19",
+    "url": "/Pokedex/static/media/defaultPokemon.bcde4333.png"
+  },
+  {
+    "revision": "dad455563bac8b9198130aa5f75fb930",
+    "url": "/Pokedex/static/media/logo.dad45556.png"
+  }
+]);
